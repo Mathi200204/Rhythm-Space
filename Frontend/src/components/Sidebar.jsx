9eec3e5fe9c1ed55f1c29763c9d3fffff6ef212a
Mathi200204 <arulkumaranmathiyalakan29@gmail.com>
2025-08-22 20:04:29 +0530

    Refactor Sidebar component; enhance search functionality and improve code readability

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { assets } from "../assets/assets.js";
 import { useNavigate } from "react-router-dom";
+import { PlayerContext } from "../context/PlayerContext";
 
 const Sidebar = () => {
-
   const navigate = useNavigate();
+  const { setSearchQuery, searchSongs } = useContext(PlayerContext);
+  const [isSearchActive, setIsSearchActive] = useState(false);
+
+  const handleSearchClick = () => {
+    setIsSearchActive(true);
+    navigate("/search");
+  };
+
   return (
     <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
       <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around ">
-        <div onClick={() =>navigate (`/`)} className="flex items-center gap-3 pl-8 cursor-pointer">
+        <div onClick={() => navigate (`/`)} className="flex items-center gap-3 pl-8 cursor-pointer">
           <img className="w-6" src={assets.home_icon} alt="" />
           <p className="font-bold">Home</p>
         </div>
-        <div className="flex items-center gap-3 pl-8 cursor-pointer">
+        <div onClick={handleSearchClick} className="flex items-center gap-3 pl-8 cursor-pointer">
           <img className="w-6" src={assets.search_icon} alt="" />
           <p className="font-bold">Search</p>
         </div>
@@ -36,7 +44,7 @@ const Sidebar = () => {
           </button>
         </div>
         <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4">
-          <h1>Let's findsome podcasts to follow </h1>
+          <h1>Let's find some podcasts to follow </h1>
           <p className="font-light">We'll keep you update on new episodes</p>
           <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
             Browse podcasts
